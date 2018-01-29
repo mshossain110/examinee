2376f4740163313a8f43445db32f28958c448054
zul.kawsar <zul.kawsar@gmail.com>
2018-01-30 04:00:05 +0600

    responsive sidemenu on added

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,5 +10,6 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
     <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
