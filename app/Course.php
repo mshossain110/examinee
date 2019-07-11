6a250834bf3202cdf7a6f539a165287d6bcd60bf
mshossain110 <mshossain110@gmail.com>
2019-07-12 01:22:29 +0600

    added course table

---
+++ app/Course.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    //
+}
---
+++ database/migrations/2019_07_11_191956_create_courses_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('price', 15, 2)->nullable();
+            $table->string('course_image')->nullable();
+            $table->date('start_date')->nullable();
+            $table->tinyInteger('published')->nullable()->default(0);
+
+            $table->integer('created_by')->unsigned();
+            
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
