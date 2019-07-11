2c868897dcaeacbe2e9d2799b56baf8daae62a06
mshossain110 <mshossain110@gmail.com>
2019-07-12 02:28:48 +0600

    Added course_student table

--- database/migrations/2019_07_11_200921_create_course_students_table.php
+++ database/migrations/2019_07_11_200921_create_course_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CourseUser extends Migration
+class CreateCourseStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CourseUser extends Migration
      */
     public function up()
     {
-        
-        Schema::create('course_user', function (Blueprint $table) {
+        Schema::create('course_students', function (Blueprint $table) {
             $table->integer('course_id')->unsigned()->nullable();
             $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            
+            $table->integer('rating')->unsigned()->default(0);
+            $table->integer('progress')->unsigned()->default(0);
+            $table->timestamps();
         });
-        
     }
 
     /**
@@ -31,6 +31,6 @@ class CourseUser extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('course_students');
     }
 }
---
+++ database/migrations/2019_07_11_201001_create_course_teachers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCourseTeachersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_teachers', function (Blueprint $table) {
+            $table->integer('course_id')->unsigned()->nullable();
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_teachers');
+    }
+}
