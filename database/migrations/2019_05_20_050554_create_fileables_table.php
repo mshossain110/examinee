e727d7a3677f9cedcf76757f9f6d0f295b696ba0
mshossain110 <mshossain110@gmail.com>
2019-08-21 12:32:19 +0600

    Added file and fileable table to manage file system

---
+++ database/migrations/2019_05_20_050554_create_fileables_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFileablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fileables', function (Blueprint $table) {
+            $table->bigInteger('file_id')->unsigned();
+            $table->morphs('fileable');
+            $table->bigInteger('project_id')->unsigned()->nullable();
+
+            $table->foreign('file_id')
+                ->references('id')
+                ->on('files')
+                ->onDelete('cascade');
+
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fileables');
+    }
+}
