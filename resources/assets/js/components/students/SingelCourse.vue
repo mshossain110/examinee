2ca56742fc3dcb9a025c8bfad99a5579cf30ff97
mshossain110 <mshossain110@gmail.com>
2019-12-11 22:48:08 +0600

    singel course font-end design

--- resources/assets/js/components/students/SingelCourse.vue
+++ resources/assets/js/components/students/SingelCourse.vue
@@ -3,48 +3,214 @@
         <section class="course_head">
             <div class="container">
                 <div class="row">
-                    <div class="col-8 course_text">
-                        <h5>Get start Vue,js Course</h5>
-                        <a href="#">Go to lara course</a>
-                        <p>In this series, step by step, I'll show you how to build web applications with Laravel 6. We'll start with the basics and incrementally dig deeper and deeper, as we review real-life examples. Once complete, you should have all the tools you need. Let's get to work</p>
-                        <div class="icon">
-                            <i class="fas fa-equals">Eiual</i>
-                            <i class="fas fa-book-open">Episode</i>
-                            <i class="fas fa-clock">Duration</i>
+                    <div class="col-8 course_text pt-3">
+                        <div class="start_text">
+                            <h5>Get start Vue.js Course</h5>
+                            <a href="#">Go to lara course</a>
+                            <p>In this series, step by step, I'll show you how to build web applications with Laravel 6. We'll start with the basics and incrementally dig deeper and deeper, as we review real-life examples. Once complete, you should have all the tools you need. Let's get to work</p>
                         </div>
-                        <div class="start_button">
+
+                        <div class="icon pt-3">
+                            <i class="fas fa-equals mr-2">Eiual</i>
+                            <i class="fas fa-book-open mr-2">Episode</i>
+                            <i class="fas fa-clock mr-2">Duration</i>
+                        </div>
+
+                        <div class="start_button pt-3">
                             <button
                                 type="button"
-                                class="btn btn-success"
+                                class="btn btn-primary mr-4"
                             >
                                 Get Start
                             </button>
                             <button
                                 type="button"
-                                class="btn btn-success"
+                                class="btn btn-primary mr-4"
                             >
                                 Add to watchlist
                             </button>
                             <button
                                 type="button"
-                                class="btn btn-success"
+                                class="btn btn-info"
                             >
-                                Add to subscride
+                                Add To Subcribe
                             </button>
                         </div>
                     </div>
-                    <div class="col-4 course_logo">
+
+                    <div class="col-4 course_logo pt-3 pl-5">
                         <img
-                            src="https://www.sunnewsonline.com/wp-content/uploads/2017/07/education.jpg"
+                            src="https://www.isasa.org/wp-content/uploads/2018/05/systemic-evaluation.jpg"
                             alt=""
                         >
-                        <h6>Let's get started</h6>
+                        <h5>Let's get started!</h5>
                     </div>
                 </div>
             </div>
         </section>
-        <section class="course_part">
-            <h1>fgf</h1>
+        <section class="lesson_part">
+            <div class="back">
+                <div class="row">
+                    <div class="col-2">
+                        <a href="#"><i class="fas fa-arrow-left mr-2" />Back To Main</a>
+                    </div>
+                    <div class="col-6">
+                        <p>Latest Episode: <a href="#">Two-Way Databinding Review</a></p>
+                    </div>
+                    <div class="col-2">
+                        <i class="fab fa-facebook-f mr-2" />
+                        <i class="fab fa-twitter" />
+                    </div>
+                    <div class="col-2 d-flex justify-content-around">
+                        <p>43% Complete</p>
+                        <i class="fas fa-eye-slash" />
+                    </div>
+                </div>
+            </div>
+            <div class="lessons">
+                <div class="row">
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex align-items-center">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                    <span class="badge badge-primary ml-3">Free</span>
+                                </div>
+
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex d-flex">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                </div>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex d-flex">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                </div>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex d-flex">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                </div>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex d-flex">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                </div>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <div
+                            class="card border-0 col-md-8 offset-md-2 d-flex flex-row align-items-center justify-content-center mb-3"
+                        >
+                            <div class="card-icon px-2 rounded-circle">
+                                <i class="fas fa-eye fa-2x" />
+                            </div>
+                            <div class="card-body">
+                                <div class="card-title d-flex d-flex">
+                                    <h4>
+                                        Light card title
+                                    </h4>
+                                    <p class="text-secondary mt-2 ml-3">
+                                        <i class="far fa-clock" />
+                                        Duation 2:40 hour
+                                    </p>
+                                </div>
+                                <p class="card-text">
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     </div>
 </template>
