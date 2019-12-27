5854dae4aa7d49281d99c3dd65011f48096bfee3
mshossain110 <mshossain110@gmail.com>
2019-12-27 17:03:54 +0600

    closed #5 import Request and Cookie class to RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 class RegisterController extends Controller
 {
