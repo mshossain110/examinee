185d7f72f5ee70576233318c82ded1201513a507
mshossain110 <mshossain110@gmail.com>
2020-05-02 03:08:28 +0600

    home single course teacher details

--- resources/assets/js/components/SingleCourse/SingleCourse.vue
+++ resources/assets/js/components/SingleCourse/SingleCourse.vue
@@ -48,6 +48,28 @@
             v-if="!loading"
             :course="course"
         />
+        <section>
+            <div class="row justify-content-center">
+                <div
+                    class="col-10"
+                >
+                    <div
+                        class="card"
+                    >
+                        <img
+                            class="card-img-top"
+                            src="/"
+                            alt="Card image cap"
+                        >
+                        <div class="card-body">
+                            <p class="card-text">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 <script>
