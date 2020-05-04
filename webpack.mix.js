39af6d1269394ea49ac1f6354b4d0f492bcc39bf
mshossain110 <mshossain110@gmail.com>
2020-05-04 12:33:14 +0600

    added ckeditor

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,6 +21,28 @@ function assetsPath (dir = '') {
 function publicPath (dir = '') {
     return path.join(__dirname, './public/', dir)
 }
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin')
+const CKEStyles = require('@ckeditor/ckeditor5-dev-utils').styles
+const CKERegex = {
+    svg: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+    css: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css/
+}
+
+Mix.listen('configReady', webpackConfig => {
+    const rules = webpackConfig.module.rules
+    const targetSVG = /(\.(png|jpe?g|gif|webp)$|^((?!font).)*\.svg$)/
+    const targetCSS = /\.css$/
+
+    // exclude CKE regex from mix's default rules
+    // if there's a better way to loop/change this, open to suggestions
+    for (const rule of rules) {
+        if (rule.test.toString() === targetSVG.toString()) {
+            rule.exclude = CKERegex.svg
+        } else if (rule.test.toString() === targetCSS.toString()) {
+            rule.exclude = CKERegex.css
+        }
+    }
+})
 
 mix.webpackConfig({
     resolve: {
@@ -33,8 +55,41 @@ mix.webpackConfig({
         }
     },
     plugins: [
+        new CKEditorWebpackPlugin({
+            language: 'en',
+            addMainLanguageTranslationsToAllAssets: true
+        }),
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: CKERegex.svg,
+                use: ['raw-loader']
+            },
+            {
+                test: CKERegex.css,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            // singleton: true,
+                            injectType: 'singletonStyleTag'
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: CKEStyles.getPostCssConfig({
+                            themeImporter: {
+                                themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+                            },
+                            minify: true
+                        })
+                    }
+                ]
+            }
+        ]
+    }
 })
 
 mix.copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', publicPath('fonts/fontawesome'))
